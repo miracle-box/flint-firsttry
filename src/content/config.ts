8b5035c18cca4f9f67a090fd92bb42f68a644d22
Nixieboluo <me@sagirii.me>
2023-01-15 23:07:12 +0800

    refactor(route): use content collections for docs

--- src/content/config.ts
+++ src/content/config.ts
@@ -12,6 +12,16 @@ const news = defineCollection({
 	slug: ({ defaultSlug }) => defaultSlug.slice(5),
 });
 
+const docs = defineCollection({
+	schema: {
+		title: z.string(),
+		desc: z.string(),
+		outdated: z.boolean().optional(),
+	},
+	// Remove the leading country code (i.e. [zh/]something.md)
+	slug: ({ defaultSlug }) => defaultSlug.split('/').splice(1, Number.POSITIVE_INFINITY).join('/'),
+});
 export const collections = {
 	news,
+	docs,
 };
---
+++ src/content/docs/en/main.md
@@ -0,0 +1,9 @@
+---
+title: Main Page
+desc: Main page of the documentation
+outdated: false
+---
+
+# Main Page
+
+idk what to write
