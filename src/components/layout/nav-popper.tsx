4ccc6fc4c214bb61038e4c748f88388c0b16bd05
Nixieboluo <me@sagirii.me>
2023-04-05 12:27:54 +0000

    chore: fix code style

--- src/components/layout/nav-popper.tsx
+++ src/components/layout/nav-popper.tsx
@@ -1,5 +1,5 @@
 import { createSignal, type JSX } from 'solid-js';
-import { Dialog, DialogPanel, Transition, TransitionChild, DialogTitle, DialogOverlay } from 'solid-headless';
+import { Dialog, DialogPanel, Transition, TransitionChild, DialogOverlay } from 'solid-headless';
 
 export default function NavPopper(props: any): JSX.Element {
 	const [isOpen, setIsOpen] = createSignal(false);
@@ -53,7 +53,12 @@ export default function NavPopper(props: any): JSX.Element {
 									class="absolute top-3 right-3 text-gray-500 hover:text-gray-700 hover:bg-gray-50 focus:ring ring-gray-100 rounded transition-colors outline-none"
 									onClick={closeModal}
 								>
-									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="24"
+										height="24"
+										viewBox="0 0 24 24"
+									>
 										<path
 											fill="currentColor"
 											d="m4.21 4.387l.083-.094a1 1 0 0 1 1.32-.083l.094.083L12 10.585l6.293-6.292a1 1 0 1 1 1.414 1.414L13.415 12l6.292 6.293a1 1 0 0 1 .083 1.32l-.083.094a1 1 0 0 1-1.32.083l-.094-.083L12 13.415l-6.293 6.292a1 1 0 0 1-1.414-1.414L10.585 12L4.293 5.707a1 1 0 0 1-.083-1.32l.083-.094l-.083.094Z"
