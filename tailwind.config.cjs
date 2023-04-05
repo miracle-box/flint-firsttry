6b263ce6f373d1c3dcc0a1568ed17cf04816cebe
Nixieboluo <me@sagirii.me>
2023-04-05 06:16:52 +0000

    feat(style): add style for shadow and blur

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -74,6 +74,29 @@ module.exports = {
 				900: '#054F31',
 			},
 		},
+		boxShadow: {
+			xs: '0px 1px 2px rgba(16, 24, 40, 0.05)',
+			sm: '0px 1px 3px rgba(16, 24, 40, 0.1), 0px 1px 2px rgba(16, 24, 40, 0.06)',
+			md: '0px 4px 8px -2px rgba(16, 24, 40, 0.1), 0px 2px 4px -2px rgba(16, 24, 40, 0.06)',
+			lg: '0px 12px 16px -4px rgba(16, 24, 40, 0.08), 0px 4px 6px -2px rgba(16, 24, 40, 0.03)',
+			xl: '0px 20px 24px -4px rgba(16, 24, 40, 0.08), 0px 8px 8px -4px rgba(16, 24, 40, 0.03)',
+			'2xl': '0px 24px 48px -12px rgba(16, 24, 40, 0.18)',
+			'3xl': '0px 32px 64px -12px rgba(16, 24, 40, 0.14)',
+		},
+		blur: {
+			none: '0',
+			sm: '4px',
+			md: '8px',
+			lg: '12px',
+			xl: '20px',
+		},
+		backdropBlur: {
+			none: '0',
+			sm: '4px',
+			md: '8px',
+			lg: '12px',
+			xl: '20px',
+		},
 		fontFamily: {
 			sans: [
 				'InterVariable',
