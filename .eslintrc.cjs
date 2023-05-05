22f132b7aec8a9aa61fea8d060fbfed69f892a64
Nixieboluo <me@sagirii.me>
2023-05-05 14:13:21 +0000

    chore(style): migrate to eslint
    I know the ESLint config is complicated, so please do not touch it and
    wait for my future ESLint shareable config project.

---
+++ .eslintrc.cjs
@@ -0,0 +1,105 @@
+const RULES = {
+	'unicorn/no-process-exit': 'off',
+	'n/file-extension-in-import': 'off',
+	'import/extensions': 'off',
+	'import/no-unassigned-import': [
+		'error',
+		{
+			allow: ['@fontsource/**', '**/*.css'],
+		},
+	],
+};
+
+const TS_RULES = {
+	'@typescript-eslint/triple-slash-reference': 'off',
+	'@typescript-eslint/no-explicit-any': 'off',
+	// Copied from XO for TypeScript files.
+	// https://github.com/xojs/xo/blob/a815db35d84b99709ac1f1de9d016d3b7d11e84a/lib/options-manager.js#L386
+	'unicorn/import-style': 'off',
+	'node/file-extension-in-import': 'off',
+	'import/export': 'off',
+	'import/default': 'off',
+	'import/named': 'off',
+};
+
+module.exports = {
+	root: true,
+	ignorePatterns: ['src/i18n/i18n-*.ts'],
+	env: {
+		node: true,
+		es2022: true,
+	},
+	parserOptions: {
+		sourceType: 'module',
+		project: 'tsconfig.json',
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	settings: {
+		'import/resolver': {
+			node: {
+				extensions: ['.js', '.cjs', '.mjs', '.jsx', '.ts', '.tsx'],
+			},
+		},
+	},
+
+	overrides: [
+		{
+			files: ['*.astro'],
+			extends: [
+				'xo',
+				'xo-typescript',
+				'./node_modules/xo/config/plugins.cjs',
+				'plugin:astro/recommended',
+				'plugin:prettier/recommended',
+			],
+			parser: 'astro-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+				extraFileExtensions: ['.astro'],
+			},
+			rules: {
+				...RULES,
+				...TS_RULES,
+				'@typescript-eslint/naming-convention': 'off',
+			},
+		},
+		{
+			files: ['*.ts'],
+			extends: [
+				'xo',
+				'xo-typescript',
+				'./node_modules/xo/config/plugins.cjs',
+				'plugin:prettier/recommended',
+			],
+			parser: '@typescript-eslint/parser',
+			rules: {
+				...RULES,
+				...TS_RULES,
+			},
+		},
+		{
+			files: ['*.tsx'],
+			extends: [
+				'xo',
+				'xo-typescript',
+				'./node_modules/xo/config/plugins.cjs',
+				'plugin:solid/recommended',
+				'plugin:prettier/recommended',
+			],
+			parser: '@typescript-eslint/parser',
+			rules: {
+				...RULES,
+				...TS_RULES,
+			},
+		},
+		{
+			files: ['*.cjs', '*.mjs', '*.js', '*.jsx'],
+			extends: ['xo', './node_modules/xo/config/plugins.cjs', 'plugin:prettier/recommended'],
+			rules: {
+				...RULES,
+			},
+		},
+	],
+};
--- src/components/layout/nav-popper.tsx
+++ src/components/layout/nav-popper.tsx
@@ -51,7 +51,7 @@ export default function NavPopper(props: any): JSX.Element {
 									<path
 										fill="currentColor"
 										d="m4.21 4.387l.083-.094a1 1 0 0 1 1.32-.083l.094.083L12 10.585l6.293-6.292a1 1 0 1 1 1.414 1.414L13.415 12l6.292 6.293a1 1 0 0 1 .083 1.32l-.083.094a1 1 0 0 1-1.32.083l-.094-.083L12 13.415l-6.293 6.292a1 1 0 0 1-1.414-1.414L10.585 12L4.293 5.707a1 1 0 0 1-.083-1.32l.083-.094l-.083.094Z"
-									></path>
+									/>
 								</svg>
 							</button>
 							{props.content}
