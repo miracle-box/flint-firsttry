3ef66d635d02448cee75888322c08d1c1438d2e8
Nixieboluo <me@sagirii.me>
2023-06-30 22:18:33 +0800

    feat: add unplugin-icons

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,8 @@
 import mdx from '@astrojs/mdx';
 import solidJs from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
+import icons from 'unplugin-icons/vite';
+// eslint-disable-next-line import/order
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
@@ -16,5 +18,12 @@ export default defineConfig({
 		shikiConfig: {
 			theme: 'github-light',
 		},
+	},
+	vite: {
+		plugins: [
+			icons({
+				compiler: 'astro',
+			}),
+		],
 	},
 });
