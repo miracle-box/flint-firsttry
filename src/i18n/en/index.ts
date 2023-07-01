d836fa4f454051eda2e99851fd86273d091280dd
Nixieboluo <me@sagirii.me>
2023-07-01 22:12:49 +0800

    feat: outdated translation prompt

--- src/i18n/en/index.ts
+++ src/i18n/en/index.ts
@@ -27,6 +27,12 @@ const en: Translation = {
 		breadcrumbBase: 'Docs',
 		fallbackTip:
 			'Requested page is not yet translated to the selected language ({0|localeName}), showing {1|localeName} version.',
+		outdatedTranslationTip: [
+			'This page contains an outdated translation of the original content. Please check the',
+			' {0|localeName} version ',
+			'for the most accurate information. ',
+			'(and consider updating the translation if you are able to help out!)',
+		],
 		tocHeading: 'On this page',
 	},
 };
