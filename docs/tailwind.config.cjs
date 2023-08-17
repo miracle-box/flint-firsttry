faf401c79f74e3b5eaf65bcc4a160410d2c1157e
Nixieboluo <me@sagirii.me>
2023-08-17 22:51:04 +0800

    fix: temp fix tailwindcss

--- docs/tailwind.config.cjs
+++ docs/tailwind.config.cjs
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'../packages/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+	],
 	theme: {
 		colors: {
 			transparent: 'transparent',
